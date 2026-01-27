1719993b05f8490d516f2dd81f754515b957b161
R0 <132775051+Eyebol@users.noreply.github.com>
2026-01-27 12:23:18 +0000

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace inplayed;
+
+using Microsoft.Web.WebView2.Core;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+		Loaded += MainWindow_Loaded;
+	}
+
+	private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+	{
+		await webView.EnsureCoreWebView2Async();
+
+		var path = System.IO.Path.Combine(
+			AppDomain.CurrentDomain.BaseDirectory,
+			"ui/index.html"
+		);
+
+		webView.Source = new Uri(path);
+
+		webView.CoreWebView2.AddHostObjectToScript(
+			"backend",
+			new Backend()
+		);
+	}
+}
+
+public class Backend
+{
+}
